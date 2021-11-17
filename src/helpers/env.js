0fc585db67d8e1e6fecdb6ac776484b2a2af7c96
figoysra <figoyusra.133@gmail.com>
2021-11-17 12:46:41 +0700

    make it better

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -11,5 +11,6 @@ const env = {
   REDIS_URL: process.env.REDIS_URL,
   REDIS_PASSWORD: process.env.REDIS_PASSWORD,
   REDIS_PORT: process.env.REDIS_PORT,
+  PORT: process.env.PORT,
 };
 module.exports = env;
